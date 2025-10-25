e6681a98fd275e481e3b18b52fd02f1347653987
sankhya <sankhyahricks@gmail.com>
2025-10-26 00:56:04 +0530

    add the memory gallery

--- component/memory.jsx
+++ component/memory.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Memory(){
   return(
     <div className = "h-[50vh] md:min-h-screen max-w-screen memory_background flex justify-center items-start scale-125 md:scale-100">
-      <img src="../src/assets/frames.png" alt="" className="md:w-[70vw] min-w-screen h-64 md:h-full"/>
+      <img src="frames.png" alt="" className="md:w-[70vw] min-w-screen h-64 md:h-full"/>
 
     </div>
   )
